cdd9a76c43297313c63c66af179a0bdf6f48ee15
KongErDong <31529011+KongErDong@users.noreply.github.com>
2020-05-26 07:17:53 +0800

    Add files via upload

---
+++ TimerEvent.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerEvent
+{
+    public int Id;
+    public string Name;
+    public float Duration;//几秒后运行
+    public float Interval;//时间间隔
+    public int Count;//运行次数
+    public object DataContext;//数据
+    public Action<object> CallBack;//函数回调
+    public Action Finished;//完成回调
+
+    public double destTime=0;//目标时间
+    public double startTime = 0;//开始时间
+    public double nowTime = 0;//当前时间
+    public double executeTime = 0;//从开始到目前为止等待了多久：按毫秒计算
+
+    DateTime baseTime = new DateTime(1970,1,1,0,0,0,0);
+    private bool isStart = false;
+    public bool isDone = false;
+    private int currentCount = 0;
+    public TimerEvent(float duration, float interval, int count, string name, int id = -1, object dataContex=null)
+    {
+        Id = id;
+        Name = name;
+        Duration = duration;
+        Interval = interval;
+        Count = count;
+        DataContext = dataContex;
+
+        startTime = GetUTCMisseconds();
+        nowTime = startTime;
+        destTime = startTime + duration * 1000;
+
+        isStart = true;
+        isDone = false;
+    }
+    /// <summary>
+    /// 单次：以后可能要取消运行的
+    /// </summary>
+    /// <param name="duration">几秒后运行</param>
+    /// <param name="callback">任务回调</param>
+    /// <param name="dataContex">数据</param>
+    /// <param name="id">id</param>
+    /// <param name="name">name</param>
+    public TimerEvent(float duration,Action<object>callback,object dataContex, int id=-1,string name=null)
+    {
+        Id = id;
+        Name = name;
+        Duration = duration;
+        CallBack = callback;
+        DataContext = dataContex;
+        Count = 1;
+       
+        Interval = 0;
+        startTime = GetUTCMisseconds();
+        nowTime = startTime;
+        destTime = startTime + duration * 1000;
+        isStart = true;
+        isDone = false;
+
+    }
+    /// <summary>
+    /// 多次运行：以后可能会取消
+    /// </summary>
+    /// <param name="duration">几秒后运行</param>
+    /// <param name="interval">时间间隔</param>
+    /// <param name="count">运行几次</param>
+    /// <param name="callback">回调</param>
+    /// <param name="dataContex">数据</param>
+    /// <param name="id">id</param>
+    /// <param name="name">name</param>
+    public TimerEvent(float duration,float interval,int count,Action<object> callback,object dataContex,int id=-1,string name=null)
+    {
+        Id = id;
+        Name = name;
+        Duration = duration;
+        Interval = interval;
+        Count = count;
+        CallBack = callback;
+        DataContext = dataContex;
+
+        startTime = GetUTCMisseconds();
+        nowTime = startTime;
+        destTime = startTime + duration * 1000;
+
+        isStart = true;
+        isDone = false;
+
+    }
+    public void Update()
+    {
+        if(isStart)
+        {
+            nowTime = GetUTCMisseconds();
+            executeTime = nowTime - startTime;
+            if(nowTime>=destTime)
+            {
+                RunCallBack();
+            }
+        }
+    }
+    double GetUTCMisseconds()
+    {
+        TimeSpan ts = DateTime.UtcNow - baseTime;
+        return ts.TotalMilliseconds;
+    }
+    void RunCallBack()
+    {
+        if(CallBack!=null)
+        {
+            CallBack(DataContext);
+            currentCount++;
+            if(currentCount>=Count)
+            {
+              
+                isDone = true;
+                if(isDone)
+                {
+                    if (Finished != null)
+                    {
+                        Finished();
+                        Reset();
+                    }
+                }
+            }
+            destTime = GetUTCMisseconds() + Interval * 1000;
+        }
+    }
+    void Reset()
+    {
+        Id = -1;
+        Name = null;
+        Duration = -1;
+        Interval = -1;
+        Count = -1;
+        currentCount = -1;
+        destTime = -1;
+        nowTime = -1;
+        executeTime=0;
+        isStart = false;
+        CallBack = null;
+        DataContext = null;
+
+    }
+}
---
+++ TimerManager.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager
+{
+    //单任务
+    private static List<TimerEvent> missonList = new List<TimerEvent>();
+    private static List<TimerEvent> missionGroupList = new List<TimerEvent>();
+
+    private static Action<object> groupTaskCallback;
+    private static bool isGroupMissonStart = false;
+    private static object DataContex;
+   
+   
+    public static void Update()
+    {
+        CheckMisson();
+    }
+    static void CheckMisson()
+    {
+        CheckSingleMisson();
+        CheckGroupMisson();
+    }
+    static void CheckSingleMisson()
+    {
+        int length = missonList.Count;
+        TimerEvent temp;
+        if (length < 1) return;
+        for (int i = length-1; i >=0; i--)
+        {
+            temp = missonList[i];
+            if(temp!=null)
+            {
+                temp.Update();
+                if(temp.isDone)
+                {
+                    missonList.RemoveAt(i);
+                }
+            }
+        }
+    }
+    static void CheckGroupMisson()
+    {
+        if (!isGroupMissonStart) return;
+        int length = missionGroupList.Count;
+        TimerEvent temp;
+        if (length < 1) return;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            temp = missionGroupList[i];
+            if(temp!=null)
+            {
+                temp.Update();
+                if(temp.isDone)
+                {
+                    missionGroupList.RemoveAt(i);
+                }
+            }
+        }
+        if(missionGroupList.Count<=0)
+        {
+            isGroupMissonStart = false;
+            if(groupTaskCallback!=null)
+            groupTaskCallback(DataContex);
+        }
+    }
+    /// <summary>
+    /// 添加：单个任务
+    /// </summary>
+    /// <param name="mission"></param>
+    public static void AddMisson(TimerEvent mission)
+    {
+        if (mission != null)
+            missonList.Add(mission);
+    }
+    /// <summary>
+    /// 添加任务组
+    /// </summary>
+    /// <param name="missons"></param>
+    /// <param name="callBack"></param>
+    /// <param name="dataContex"></param>
+    public static void AddMissons(TimerEvent[] missons, Action<object> callBack = null, object dataContex = null)
+    {
+        if (missons != null)
+        {
+            missionGroupList.AddRange(missons);
+            groupTaskCallback = callBack;
+            isGroupMissonStart = true;
+            DataContex = dataContex;
+        }
+
+    }
+    /// <summary>
+    /// 删除
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="isGroup"></param>
+    public static void DeleteMisson(string name,bool isGroup=false)
+    {
+        if(!isGroup)
+        {
+            DeleMisson(name);
+        }
+        else
+        {
+            DeleMissonOfGroup(name);
+        }
+      
+    }
+    /// <summary>
+    /// 替换:限于单次运行的任务
+    /// </summary>
+    /// <param name="newOne"></param>
+    /// <param name="isGroup"></param>
+    public static void ReplaceMission(TimerEvent newOne, bool isGroup = false)
+    {
+        if (!isGroup)
+        {
+            ReplaceMisson(newOne);
+        }
+        else
+        {
+            ReplaceMissonOfGroup(newOne);
+        }
+    }
+    /// <summary>
+    /// 延迟执行：单次运行的任务
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="delayTime"></param>
+    /// <param name="isGroup"></param>
+    public static void DelayMission(string name,float delayTime,bool isGroup=false)
+    {
+        if(!isGroup)
+        {
+            DelayMission(name,delayTime);
+        }
+        else
+        {
+            DelayMissionOfGroup(name,delayTime);
+        }
+    }
+
+    static void DeleMisson(string name)
+    {
+        if (name == null) return;
+        int length = missonList.Count;
+        if (length < 1) return;
+        TimerEvent temp;
+        for (int i = length-1; i >=0; i--)
+        {
+            temp = missonList[i];
+            if(name.Equals(temp.Name))
+            {
+                missonList.RemoveAt(i);
+                break;
+            }
+        }
+        Debug.Log(missonList.Count);
+        Debug.Log("删除了>>>>>>");
+    }
+    static void DeleMissonOfGroup(string name)
+    {
+        if (name == null) return;
+        int length = missionGroupList.Count;
+        if (length < 1) return;
+        TimerEvent temp;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            temp = missionGroupList[i];
+            if (name.Equals(temp.Name))
+            {
+                missionGroupList.RemoveAt(i);
+                break;
+            }
+        }
+    }
+    static void ReplaceMisson(TimerEvent newOne)
+    {
+        int length = missonList.Count;
+        if (newOne == null) return;
+        if (newOne.Name == null) return;
+        TimerEvent temp;
+        for (int i = 0; i < length; i++)
+        {
+            temp = missonList[i];
+            if (newOne.Name.Equals(temp.Name))
+            {
+                temp.CallBack = newOne.CallBack;
+                temp.DataContext = newOne.DataContext;
+                temp.destTime = temp.startTime + newOne.Duration * 1000;
+                break;
+             }
+        }
+    }
+    static void ReplaceMissonOfGroup(TimerEvent newOne)
+    {
+        int length = missionGroupList.Count;
+        if (newOne == null) return;
+        if (newOne.Name == null) return;
+        TimerEvent temp;
+        for (int i = 0; i < length; i++)
+        {
+            temp = missionGroupList[i];
+            if (newOne.Name.Equals(temp.Name))
+            {
+                temp.CallBack = newOne.CallBack;
+                temp.DataContext = newOne.DataContext;
+                break;
+            }
+        }
+    }
+    static void DelayMission(string name,float delayTime)
+    {
+        int length = missonList.Count;
+      
+        if (name == null) return;
+        TimerEvent temp;
+        for (int i = 0; i < length; i++)
+        {
+            temp = missonList[i];
+            if (name.Equals(temp.Name))
+            {
+                temp.destTime += delayTime * 1000;
+                break;
+            }
+        }
+    }
+    static void DelayMissionOfGroup(string name, float delayTime)
+    {
+        int length = missionGroupList.Count;
+
+        if (name == null) return;
+        TimerEvent temp;
+        for (int i = 0; i < length; i++)
+        {
+            temp = missionGroupList[i];
+            if (name.Equals(temp.Name))
+            {
+                temp.destTime += delayTime * 1000;
+                break;
+            }
+        }
+    }
+
+    public static void ClearAllMission()
+    {
+        
+        int length = missonList.Count;
+        if(length>0)
+        {
+            for (int i = length - 1; i > +0; i--)
+            {
+                missonList[i] = null;
+            }
+            missonList.Clear();
+        }
+        length = missionGroupList.Count;
+        if (length > 0)
+        {
+            for (int i = length - 1; i > +0; i--)
+            {
+                missionGroupList[i] = null;
+            }
+            missionGroupList.Clear();
+        }
+
+
+    }
+}
